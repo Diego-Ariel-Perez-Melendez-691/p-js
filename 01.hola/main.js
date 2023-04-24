b8db5f8c2986ca3aba3b4b45bea2822b31fffe1b
pemd060928hmcrlga4@soycecytem.mx <davi123456789ruiz@gmail.com>
2023-04-24 09:16:34 -0600

    hola

---
+++ 01.hola/main.js
@@ -0,0 +1,12 @@
+const Hola=()=>{
+ let nombre=prompt("¿cual es su nombre?");
+ alert("Hola Mundo " + nombre+" ");
+ 
+}
+const sumar=()=>{
+  let número1=parseInt(prompt("Número1:"));
+  let número2=parseInt(prompt("Número2:"));
+  let suma= número1+número2;
+  alert("El resultado es: "+suma);
+  
+}
---
+++ 04.foto/app.js
@@ -0,0 +1,23 @@
+const uno=()=>{
+
+let img=document.querySelector("#ima");
+
+img.src="img.jpg";
+
+}
+
+const dos=()=>{
+
+let img=document.querySelector("#ima");
+
+img.src="img2.jpg";
+
+}
+
+const tres=()=> {
+
+let img = document.querySelector("#ima");
+
+img.src ="img3.jpg";
+
+}
